3a067d3c8f86d81ce39c27aa444c6dc4f913182d
Yousef Janajri <yfj211@gmail.com>
2016-02-09 10:00:04 -0500

    Update pipe.js

--- drs-node/src/pipe.js
+++ drs-node/src/pipe.js
@@ -49,7 +49,7 @@ export default class Pipe {
 				const conn = await this._route(cmd.action)
 				await conn.send(cmd)
 			} catch (ex) {
-				console.log(ex)
+				console.log('out', ex)
 				await timeout(1000)
 				continue
 			}
@@ -105,7 +105,7 @@ export default class Pipe {
 				const cmd = this.protocol.decode(data)
 				await this._process(conn, cmd)
 			} catch (ex) {
-				console.log(ex)
+				console.log('in', ex)
 			}
 		}.bind(this))
 	}
