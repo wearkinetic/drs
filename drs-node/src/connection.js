e5aea66a24189962fd2643ff6d2b8be7a490d9e6
Dax Ravi <d@ironbay.digital>
2016-03-08 01:48:38 -0500

    Rewrite

--- drs-node/src/connection.js
+++ drs-node/src/connection.js
@@ -1,20 +1,36 @@
-export default class Connection {
-	constructor(rw, protocol) {
-		this.raw = rw
+import UUID from './uuid'
+import { Error, Exception } from './error'
+
+export class Connection {
+	constructor(raw, protocol) {
+		this.cache = {}
+		this._raw = raw
 		this._protocol = protocol
-		this._cache = {}
-	}
-
-	set(key, value) {
-		this._cache[key] = value
-	}
-
-	get(key) {
-		return this._cache[key]
+		this._pending = {}
 	}
 
 	async send(cmd) {
-		const data = await this._protocol.encode(cmd)
-		this.raw.send(data)
+		if (!cmd.key)
+			cmd.key = UUID.ascending()
+		const result = await new Promise(resolve => {
+			this._pending[cmd.key] = resolve
+			this.fire(cmd)
+		})
+		if (result.action === 'drs.error')
+			throw new Error(result.body)
+		if (result.action === 'drs.exception')
+			throw new Exception(result.body)
+		return result.body
+	}
+
+	fire(cmd) {
+		if (!cmd.key)
+			cmd.key = UUID.ascending()
+		this._raw.send(this._protocol.encode(cmd))
+	}
+
+	static async dial(transport, protocol, host) {
+		const raw = await transport.dial(host)
+		return new Connection(raw, protocol)
 	}
 }
--- drs-node/src/protocol/json.js
+++
@@ -1,8 +0,0 @@
-export default {
-	encode(body) {
-		return JSON.stringify(body)
-	},
-	decode(data) {
-		return JSON.parse(data)
-	}
-}
