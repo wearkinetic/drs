8c40120f9cdc750b9fb2a7598d763a0a3028c560
Dax Ravi <d@ironbay.digital>
2016-04-10 01:43:36 -0400

    Is this really ok

--- drs-go/conn_test.go
+++ drs-go/conn_test.go
@@ -2,7 +2,6 @@ package drs
 
 import (
 	"testing"
-	"time"
 
 	"github.com/ironbay/drs/drs-go/protocol"
 	"github.com/ironbay/drs/drs-go/transports/ws"
@@ -12,19 +11,17 @@ import (
 func TestConn(t *testing.T) {
 	conn := NewConnection2()
 	transport := ws.New(dynamic.Empty())
-	go conn.Dial(protocol.JSON, transport, "localhost:12000")
-	/*
-		count := 0
-			for {
-				conn.Fire(&Command{
-					Action: "drs.ping",
-				})
-				count++
-				if count > 5 {
-					break
-				}
-			}
-	*/
-	time.Sleep(5 * time.Second)
+	go conn.Dial(protocol.JSON, transport, "delta.wearkinetic.com")
+	count := 0
+	for {
+		conn.Fire(&Command{
+			Action: "drs.ping",
+			Body:   count,
+		})
+		count++
+		if count > 1000 {
+			break
+		}
+	}
 	conn.Close()
 }
