35c36ecb804b8a1e0e81302a8e6b99c375ce20c7
Dax Ravi <d@ironbay.digital>
2016-05-16 23:20:00 -0400

    Simplify

--- drs-go/connection_test.go
+++ drs-go/connection_test.go
@@ -1,15 +1,23 @@
 package drs
 
-/*
+import (
+	"log"
+	"testing"
+
+	"github.com/ironbay/drs/drs-go/protocol"
+	"github.com/ironbay/drs/drs-go/transports/ws"
+)
+
 func TestConnection(t *testing.T) {
 	transport := ws.New(map[string]interface{}{"token": "djkhaled"})
-	server := NewServer(transport)
-	go server.Listen()
-	conn := NewConnection(protocol.JSON)
-	go conn.Dial(transport, "localhost:12000", true)
-	result, _ := conn.Request(&Command{
+	conn, err := Dial(protocol.JSON, transport, "delta.inboxtheapp.com")
+	log.Println("Connected")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go conn.Read()
+	result, err := conn.Call(&Command{
 		Action: "drs.ping",
 	})
-	log.Println("Pinged", result)
+	log.Println(result)
 }
-*/
--- drs-go/processor.go
+++ drs-go/processor.go
@@ -1,121 +1,80 @@
 package drs
 
 import (
-	"fmt"
-	"log"
-	"runtime/debug"
-
-	"github.com/ironbay/delta/uuid"
 	"github.com/ironbay/dynamic"
 	"github.com/streamrail/concurrent-map"
 )
 
 type Message struct {
-	Command    *Command
-	Connection *Connection
-	Context    map[string]interface{}
+	Conn    *Connection
+	Command *Command
+	Context map[string]interface{}
 }
 
 type Processor struct {
 	handlers map[string][]func(*Message) (interface{}, error)
 	pending  cmap.ConcurrentMap
-	stats    cmap.ConcurrentMap
 }
 
-type Stats struct {
-	Errors     int64 `json:"errors"`
-	Exceptions int64 `json:"exceptions"`
-	Success    int64 `json:"success"`
-}
-
-func newProcessor() *Processor {
+func NewProcessor() *Processor {
 	return &Processor{
-		handlers: map[string][]func(*Message) (interface{}, error){},
+		handlers: make(map[string][]func(*Message) (interface{}, error)),
 		pending:  cmap.New(),
-		stats:    cmap.New(),
 	}
 }
 
-func (this *Processor) On(action string, handlers ...func(*Message) (interface{}, error)) error {
-	exists, ok := this.handlers[action]
-	if ok {
-		exists = append(exists, handlers...)
-		return nil
-	}
-	this.handlers[action] = handlers
-	return nil
+func (this *Processor) Enqueue(key string) chan *Command {
+	block := make(chan *Command)
+	this.pending.Set(key, block)
+	return block
 }
 
-func (this *Processor) wait(cmd *Command, cb func()) (interface{}, error) {
-	if cmd.Key == "" {
-		cmd.Key = uuid.Ascending()
-	}
-	wait := make(chan *Command, 1)
-	this.pending.Set(cmd.Key, wait)
-	cb()
-	response := <-wait
-	if response.Action == ERROR {
-		return nil, &DRSError{
-			Message: fmt.Sprint(response.Body),
-		}
-	}
-	if response.Action == EXCEPTION {
-		return nil, &DRSException{
-			Message: fmt.Sprint(response.Body),
-		}
-	}
-	return response.Body, nil
+func (this *Processor) On(action string, cb ...func(*Message) (interface{}, error)) {
+	this.handlers[action] = cb
 }
 
-func (this *Processor) process(cmd *Command, conn *Connection) {
-	if cmd.Action == RESPONSE || cmd.Action == ERROR || cmd.Action == EXCEPTION {
-		waiting, ok := this.pending.Get(cmd.Key)
-		if ok {
-			waiting.(chan *Command) <- cmd
-			this.pending.Remove(cmd.Key)
-		}
-		return
-	}
-	res, err := this.Trigger(cmd, conn)
-	conn.respond(cmd, res, err)
-}
-
-func (this *Processor) Trigger(cmd *Command, conn *Connection) (result interface{}, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println(r)
-			log.Println(string(debug.Stack()))
-			err = r.(error)
-		}
-	}()
-	handlers, ok := this.handlers[cmd.Action]
-	if !ok {
-		handlers, ok = this.handlers["*"]
+func (this *Processor) Process(conn *Connection, cmd *Command) {
+	if cmd.Action == ERROR || cmd.Action == EXCEPTION || cmd.Action == RESPONSE {
+		match, ok := this.pending.Get(cmd.Key)
 		if !ok {
-			return nil, Error("No handlers for this action")
+			return
 		}
+		this.pending.Remove(cmd.Key)
+		match.(chan *Command) <- cmd
 	}
-	msg := &Message{
-		Context:    dynamic.Empty(),
-		Command:    cmd,
-		Connection: conn,
+	resp, err := this.Invoke(conn, cmd)
+	conn.respond(cmd.Key, resp, err)
+}
+
+func (this *Processor) Invoke(conn *Connection, cmd *Command) (interface{}, error) {
+	message := &Message{
+		Conn:    conn,
+		Command: cmd,
+		Context: dynamic.Empty(),
 	}
-	for _, h := range handlers {
-		result, err = h(msg)
+	handlers := this.handlers[cmd.Action]
+	if handlers == nil {
+		return nil, Error("No handlers for " + cmd.Action)
+	}
+	var result interface{}
+	var err error
+	for _, cb := range handlers {
+		result, err = cb(message)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return result, err
+	return result, nil
 }
 
-func (this *Processor) clear() {
+func (this *Processor) Clear() {
 	for kv := range this.pending.Iter() {
 		kv.Val.(chan *Command) <- &Command{
-			Action: "drs.exception",
-			Body: map[string]interface{}{
-				"message": "Connection closed",
-			},
+			Key:    kv.Key,
+			Action: ERROR,
+			Body: dynamic.Build(
+				"message", "Connection closed",
+			),
 		}
 		this.pending.Remove(kv.Key)
 	}
