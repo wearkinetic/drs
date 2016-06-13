ad424fbfd53a0af7a4af3df81d2b779ed3f26826
Yousef Janajri <yfj211@gmail.com>
2016-06-13 13:58:02 -0400

    Update connection.js

--- drs-node/src/connection.js
+++ drs-node/src/connection.js
@@ -15,8 +15,7 @@ export default class Connection {
 		this._pending = {}
 		this._processor = new Processor()
 		this.on = this._processor.on.bind(this._processor)
-
-		this._interval = setInterval(() => this._ping(), 1000)
+		this._interval = setInterval(() => this._ping(), 60 * 60 * 1000)
 		this._ping()
 	}
 
