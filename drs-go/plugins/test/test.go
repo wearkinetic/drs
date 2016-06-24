164a12dd1931581ee06b26e0fe85c6bb7f670d7b
Yousef Janajri <yfj211@gmail.com>
2016-06-24 11:12:04 -0400

    Create test.go

---
+++ drs-go/plugins/test/test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"time"
+
+	"github.com/ironbay/drs/drs-go"
+)
+
+func Attach(processor *drs.Processor) {
+	processor.On(
+		"drs.kaboom",
+		func(msg *drs.Message) (interface{}, error) {
+			return nil, drs.Exception(rune(0x1F4A5))
+		},
+	)
+}
