77848f64fcc0844504f546cc455f539a7ffc7c5b
Yousef Janajri <yfj211@gmail.com>
2016-06-24 11:14:39 -0400

    Update debug.go

--- drs-go/plugins/debug/debug.go
+++ drs-go/plugins/debug/debug.go
@@ -6,11 +6,13 @@ import (
 	"github.com/ironbay/drs/drs-go"
 )
 
+const KABOOM = rune(0x1F4A5)
+
 func Attach(processor *drs.Processor) {
 	processor.On(
 		"drs.kaboom",
 		func(msg *drs.Message) (interface{}, error) {
-			return nil, drs.Exception(rune(0x1F4A5))
+			return nil, drs.Exception(string(KABOOM))
 		},
 	)
 }
